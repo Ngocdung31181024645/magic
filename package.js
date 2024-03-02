a3e574accc43a5a663a27957d76373c25a45a70b
Anne031120 <93469897+Ngocdung31181024645@users.noreply.github.com>
2024-03-03 00:23:24 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,27 @@
+{
+  "name": "magic",
+  "description": "Magic Method/getter/setters for Node.JS (Basic ES5 Harmony Proxies)",
+  "keywords": ["harmony", "proxy", "proxies", "__get", "__set", "__call", "getter", "setter"],
+  "version": "1.0.3",
+  "homepage": "http://aikar.co/magic",
+  "repository": {
+    "type": "git",
+    "url": "git://github.com/aikar/magic.git"
+  },
+  "author": "Aikar <aikar@aikar.co>",
+  "main": "lib/magic.js",
+  "directories": {
+    "lib": "lib"
+  },
+  "devDependencies": {
+    "vows": ""
+  },  
+  "scripts": {
+    "test": "./runTest.js",
+    "preinstall": "make makelibs",
+    "preuninstall": "make clean"
+  },
+  "engines": {
+    "node": ">=0.4.0"
+  }
+}
